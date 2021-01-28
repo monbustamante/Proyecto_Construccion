99201a5afb7a528be7fe8eb1a175738290766f41
monbustamante <montse.bustamante.r@gmail.com>
2021-01-27 21:08:13 -0500

    Created Clase Matriz

---
+++ ProyectoMatricesConstruccion/src/modelo/Matriz.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Equipo Los Aprobados ;) 
+ */
+public class Matriz {
+    private final int M;             // number of rows
+    private final int N;             // number of columns
+    private final double[][] data;  
+    
+    public Matriz(int)
+}
