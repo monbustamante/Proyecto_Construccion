d6328e991bfdd6979d185087e0815622a5eacb85
monbustamante <montse.bustamante.r@gmail.com>
2021-01-28 12:29:17 -0500

    Added JavaDoc comments

---
+++ ProyectoMatricesConstruccion/README.txt
@@ -0,0 +1,5 @@
+Construcción del Software - Documentación Proyecto Final
+
+TÍTULO DEL PROYECTO: Calculadora de Matrices
+
+Autores: Jafet Alejandro Fernández Martínez, Víctor Enrique Cauich Dávalos y Montserrat Bustamante Rentería
--- ProyectoMatricesConstruccion/src/modelo/Matriz.java
+++ ProyectoMatricesConstruccion/src/modelo/Matriz.java
@@ -6,7 +6,8 @@
 package modelo;
 
 /**
- *
+ * Esta clase representa las matrices con las que se realizarán las operaciones.
+ * @version 1.0, 27/01/2021
  * @author Equipo Los Aprobados ;) 
  */
 
@@ -15,18 +16,32 @@ public class Matriz {
     private int numeroColumnas;
     private int[][] datos;
 
+    /*
+     * @return número de tipo de entero que representa el número de filas de la matriz.
+    */
     public int getNumeroFilas() {
         return numeroFilas;
     }
-
+    
+    /*
+     * Establece el número de filas de la matriz.
+     * @param número de tipo entero numeroFilas
+    */
     public void setNumeroFilas(int numeroFilas) {
         this.numeroFilas = numeroFilas;
     }
-
+    
+    /*
+     * @return número de tipo de entero que representa el número de columnas de la matriz.
+    */
     public int getNumeroColumnas() {
         return numeroColumnas;
     }
 
+    /*
+     * Establece el número de columnas de la matriz.
+     * @param número de tipo entero numeroColumnas
+    */
     public void setNumeroColumnas(int numeroColumnas) {
         this.numeroColumnas = numeroColumnas;
     }
@@ -40,6 +55,15 @@ public class Matriz {
         //Llenado de matriz
     }
     
+    
+    /**
+     * Este método indica si la Matriz ingresada es cuadrada, es decir,
+     * que su número de filas y de columnas es el mismo.
+     * Esta función se utilizará para verificar que las Matrices cumplen con esta propiedad
+     * para que algunas de las operaciones puedan realizarse.
+     * @param Una matriz M.
+     * @return true si la matriz es cuadrada, false si no es cuadrada.
+    */
     public boolean esCuadrada(Matriz M){
         if(M.getNumeroColumnas() == M.getNumeroFilas()){
             return true;
