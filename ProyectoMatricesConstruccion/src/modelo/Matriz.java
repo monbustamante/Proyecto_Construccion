1cc86cbfecd130988678352f1e709e67799e216e
monbustamante <montse.bustamante.r@gmail.com>
2021-01-27 21:22:49 -0500

    Update Matriz.java

--- ProyectoMatricesConstruccion/src/modelo/Matriz.java
+++ ProyectoMatricesConstruccion/src/modelo/Matriz.java
@@ -9,12 +9,43 @@ package modelo;
  *
  * @author Equipo Los Aprobados ;) 
  */
+
 public class Matriz {
-    private int numeroFilas;             // number of rows
-    private int numeroColumnas;             // number of columns  
+    private int numeroFilas;             
+    private int numeroColumnas;
+    private int[][] datos;
+
+    public int getNumeroFilas() {
+        return numeroFilas;
+    }
+
+    public void setNumeroFilas(int numeroFilas) {
+        this.numeroFilas = numeroFilas;
+    }
+
+    public int getNumeroColumnas() {
+        return numeroColumnas;
+    }
+
+    public void setNumeroColumnas(int numeroColumnas) {
+        this.numeroColumnas = numeroColumnas;
+    }
     
     public Matriz(int numeroColumnas, int numeroFilas){
         this.numeroColumnas = numeroColumnas;
         this.numeroFilas = numeroFilas;
+    }
+    
+    public Matriz(int[][] datos){
+        //Llenado de matriz
+    }
+    
+    public boolean esCuadrada(Matriz M){
+        if(M.getNumeroColumnas() == M.getNumeroFilas()){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
