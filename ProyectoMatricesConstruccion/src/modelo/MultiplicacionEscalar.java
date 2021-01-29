f1a10cf2d5426a8901228fd26302ec0fb5dddef3
monbustamante <montse.bustamante.r@gmail.com>
2021-01-29 10:10:07 -0500

    Update EscalarMatrices.java
    Corrected multiplicarPorEscalar method and added imprimirMatriz method

--- ProyectoMatricesConstruccion/src/modelo/MultiplicacionEscalar.java
+++ ProyectoMatricesConstruccion/src/modelo/MultiplicacionEscalar.java
@@ -6,20 +6,45 @@
 package modelo;
 
 /**
- *
- * @author monts
+ * Esta clase realiza la multiplicación de una matriz por un escalar.
+ * @version 1.1
+ * @author Montserrat Bustamante Rentería
  */
 public class MultiplicacionEscalar {
     private int escalar;
     
-    public int multiplicarPorEscalar(int[][] matriz, int escalar){
-        for(int i = 0; i < matriz.length ; i++){
-            for(int j = 0; j < matriz[0].length; j++){
-                System.out.format("%d \t", (escalar * matriz[i][j]));
+    /*
+     * Esta función toma la matriz de tipo entero y la multiplica por el escalar
+     * @param matriz Matriz de enteros
+     * @param escalar Número entero que se multiplicará por la matriz
+     * @return matrizResultado Matriz resultante de la multiplicación de la matriz por escalar
+    */
+    public int[][] multiplicarPorEscalar(int[][] matriz, int escalar){
+        int numFilas = matriz.length;
+        int numColumnas = matriz[0].length;
+        int[][] matrizResultado;
+        
+        for(int i = 0; i < numFilas ; i++){
+            for(int j = 0; j < numColumnas; j++){
+               matrizResultado[i][j] = escalar * matriz[i][j];
             }
-	System.out.print("\n");
 	}
-        return 0;
+        return matrizResultado;
+    }
+    
+    /*
+     * Esta función muestra el resultado de la multiplicación de la matriz por el escalar.
+    */
+    public void imprimirMatriz(String matrizTittle, int[][] matriz){
+        int filas = matriz.length;
+        int columnas = matriz[0].length;
+        System.out.println(matrizTittle);
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(matriz[i][j] + "   ");
+            }
+            System.out.println("");
+        }    
     }
     
 }
